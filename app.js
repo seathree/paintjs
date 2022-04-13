b49a9ef934ba42030b9c1ec43df57234cf639100
seathree <park.yang.ho@gmail.com>
2022-04-14 08:25:58 +0900

    #2.1 2D Context

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 
