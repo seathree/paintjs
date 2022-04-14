030cfeeaef14d233f5e795fab1d303a7d7843684
seathree <park.yang.ho@gmail.com>
2022-04-14 09:51:04 +0900

    Final

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ canvas.height = CANVAS_SIZE;
 
 ctx.fillStyle = "white";
 ctx.fillRect(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
